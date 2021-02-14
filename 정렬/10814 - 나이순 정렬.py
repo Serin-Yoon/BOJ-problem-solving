9f2ca5c1e6774ddfe209397163adfe4bb616394a
Serin <bcode429@gmail.com>
2021-02-14 23:48:52 +0900

    week 5

---
+++ 정렬/10814 - 나이순 정렬.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+member = []
+
+for i in range(N):
+    age, name = input().split()
+    member.append((int(age), name))
+
+member.sort(key=lambda x: (x[0]))
+
+for i in range(N):
+    print(member[i][0], member[i][1])
+
